e4d4a5e8a6d6de11e835267c49d88106d4240af4
JK11751 <kgimase@gmail.com>
2024-11-07 11:02:27 +0300

    railway updates

--- jenga_bay/backend/settings.py
+++ jenga_bay/backend/settings.py
@@ -28,9 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://precious-adversely-prawn.ngrok-free.app'
-]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
@@ -147,10 +145,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'C:/Users/Gimase/Desktop/Repos/JengaBay/JengaBay_frontend/build')
-]
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [
+#  os.path.join(BASE_DIR, 'C:/Users/Gimase/Desktop/Repos/JengaBay/JengaBay_frontend/build')
+#]
+
+STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
